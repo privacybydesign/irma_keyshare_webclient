039c121b934d3553225637543e1757c0c101143a
David Venhoek <d.venhoek@privacybydesign.foundation>
2020-06-03 11:38:54 +0200

    Implemented URL fragment handling.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import buildStore from './store';
+
+store = buildStore()
+
+const fragment = window.location.hash
+if (fragment.startsWith('#token=')) {
+  const token = fragment.substr(7);
+  store.dispatch({type: 'startTokenLogin', token: token});
+}
+if (fragment.startsWith('#verify=')) {
+  const token = fragment.substr(8);
+  store.dispatch({type: 'startRegistrationVerify', token: token});
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
