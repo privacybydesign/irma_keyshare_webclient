c78fc96d9a1f8186a976011dca5862b224969781
Ivar Derksen <ivarderksen@hotmail.nl>
2020-06-17 12:00:16 +0200

    Use plain css for styling

--- src/helpers/irma_app_bar.js
+++ src/helpers/irma_app_bar.js
@@ -1,15 +1,14 @@
-import Toolbar from '@material-ui/core/Toolbar';
-import AppBar from '@material-ui/core/AppBar';
 import React from 'react';
 
+import './irma_app_bar.css'
 
 const IrmaAppBar = (props) => {
   return (
-    <AppBar position={'relative'}>
-      <Toolbar style={{justifyContent: 'center'}}>
+    <header>
+      <h1>
         {props.title}
-      </Toolbar>
-    </AppBar>
+      </h1>
+    </header>
   );
 };
 
