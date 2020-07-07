543f051f7a729f0f12a1ba2bf20ac8f32a0d4047
Ivar Derksen <ivarderksen@hotmail.nl>
2020-07-08 01:40:55 +0200

    Introduced registration verified page and added stub for account overview page

---
+++ src/helpers/success_icon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SuccessIcon = ({className}) => {
+  return (
+    <svg className={className} viewBox="0 0 396 396" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M396 198C396 307.352 307.352 396 198 396C88.6476 396 0 307.352 0 198C0 88.6476 88.6476 0 198 0C307.352 0 396 88.6476 396 198Z" fill="#449832"/>
+      <path d="M293.167 133.273L161.625 264.814L101.833 205.023" stroke="white" strokeWidth="22" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+
+export default SuccessIcon;
