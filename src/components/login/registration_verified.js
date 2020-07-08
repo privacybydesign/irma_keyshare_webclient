a5b0e421a5615257047bb8b5ec61dd44deb51dd3
Ivar Derksen <ivarderksen@hotmail.nl>
2020-07-08 13:17:53 +0200

    Make registration verified screen dependent on device type

--- src/components/login/registration_verified.js
+++ src/components/login/registration_verified.js
@@ -6,10 +6,32 @@ import IrmaAppBar from '../../helpers/irma_app_bar';
 import SuccessIcon from '../../helpers/success_icon';
 
 const RegistrationVerified = (props) => {
-  function continueToMyIrma(event) {
+  const continueToMyIrma = (event) => {
     event.preventDefault();
     window.location.hash = '';
     props.dispatch({type: 'loggedIn'});
+  }
+
+  const isMobile = () => {
+    // IE11 doesn't have window.navigator, test differently
+    // https://stackoverflow.com/questions/21825157/internet-explorer-11-detection
+    if (!!window.MSInputMethodContext && !!document.documentMode)
+      return false;
+
+    if (/Android/i.test(window.navigator.userAgent)) {
+      return true;
+    }
+
+    // https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
+    if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream)
+      return true;
+
+    // https://stackoverflow.com/questions/57776001/how-to-detect-ipad-pro-as-ipad-using-javascript
+    if (/Macintosh/.test(navigator.userAgent) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2)
+      return true;
+
+    // Neither Android nor iOS, assuming desktop
+    return false;
   }
 
   return (
@@ -24,18 +46,18 @@ const RegistrationVerified = (props) => {
           {props.t('explanation')}
         </p>
         <p>
-          <a className={'return-button'} href={'https://irma.app/-/'}>
-            <img src={process.env.PUBLIC_URL + '/assets/irma-logo.svg'}
-                 className={'irma-logo'}
-                 alt={'irma-logo'}
-            />
-            <span>
-              {props.t('return-to-irma')}
-            </span>
-          </a>
-        </p>
-        <p>
-          {props.t('app-is-ready')}
+          {isMobile()
+            ? <a className={'return-button'} href={'https://irma.app/-/'}>
+                <img src={process.env.PUBLIC_URL + '/assets/irma-logo.svg'}
+                     className={'irma-logo'}
+                     alt={'irma-logo'}
+                />
+                <span>
+                  {props.t('return-to-irma')}
+                </span>
+              </a>
+            : props.t('app-is-ready')
+          }
         </p>
         <p><a href={'/#'} onClick={continueToMyIrma}>
           {props.t('continue-to-myirma')}
