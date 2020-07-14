c2d6c629e6813662305b50d00c7cdb777f14c323
Ivar Derksen <ivarderksen@hotmail.nl>
2020-07-14 13:29:11 +0200

    Introduced logs table in account overview

--- src/components/account-overview/index.js
+++ src/components/account-overview/index.js
@@ -3,25 +3,53 @@ import {withTranslation} from 'react-i18next';
 import IrmaAppBar from '../../helpers/irma_app_bar';
 
 import '../../templates/column.scss';
+import {connect} from 'react-redux';
+import LoadingSpinner from '../../helpers/loading_spinner';
+import Logs from './logs';
+
+const mapStateToProps = state => {
+  return {
+    ...state.userdata,
+  };
+};
 
 class AccountOverview extends React.Component {
   t = this.props.t;
+
+  renderUsername() {
+    return (
+      <p>
+        {this.t('logged-in-as')}
+        &nbsp;
+        <span>{this.props.username}</span>
+      </p>
+    );
+  }
+
+  renderBody() {
+    if (this.props.fetching) {
+      return <LoadingSpinner/>;
+    } else {
+      return (
+        <div className={'column'}>
+          {this.renderUsername()}
+          <Logs context={this.props.context}/>
+        </div>
+      );
+    }
+  }
 
   render() {
     return (
       <>
         <IrmaAppBar
-          title={this.t('common:title')}
+          title={this.t('title')}
           onLogout={() => this.props.dispatch({type: 'loggedOut'})}
         />
-        <div className={'column'}>
-          <p>
-            TODO.
-          </p>
-        </div>
+        {this.renderBody()}
       </>
     );
   }
 }
 
-export default withTranslation(['account-overview', 'common'])(AccountOverview);
+export default connect(mapStateToProps)(withTranslation('account-overview')(AccountOverview));
