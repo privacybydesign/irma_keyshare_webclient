59224ebb81f07e51be3a7139d40f9c74ff541f36
Ivar Derksen <ivarderksen@hotmail.nl>
2020-07-15 14:24:07 +0200

    Fixed indents and camel case consistency

--- src/components/emails/index.js
+++ src/components/emails/index.js
@@ -5,48 +5,62 @@ import {withTranslation} from 'react-i18next';
 import IrmaButton from '../../helpers/irma_button';
 
 function mapStateToProps(state) {
-    return {
-        emails: state.userdata.emails,
-    };
+  return {
+    emails: state.userdata.emails,
+  };
 }
 
 class Emails extends React.Component {
-    t = this.props.t;
+  t = this.props.t;
 
-    delete_email = (address) => {
-        return () => this.props.dispatch({
-            type: 'removeEmail',
-            email: address,
-        });
-    }
+  deleteEmail(address) {
+    return () => this.props.dispatch({
+      type: 'removeEmail',
+      email: address,
+    });
+  }
 
-    render_email_header = () => {
-        return (<thead>
-            <tr><th>{this.t('emailaddress')}</th></tr>
-        </thead>);
-    }
+  renderEmailHeader() {
+    return (
+      <thead>
+      <tr>
+        <th>{this.t('emailaddress')}</th>
+      </tr>
+      </thead>
+    );
+  }
 
-    render_email_row = (address) => {
-        return (<tr>
-            <td>{address.email}</td>
-            <td>{address.delete_in_progress?this.t('delete_in_progress'):<IrmaButton theme={'secondary'} onClick={this.delete_email(address.email)}>{this.t('delete')}</IrmaButton>}</td>
-        </tr>);
-    }
+  renderEmailRow (address) {
+    return (
+      <tr key={address.email}>
+        <td>{address.email}</td>
+        <td>{
+          address.delete_in_progress
+            ? this.t('delete_in_progress')
+            : <IrmaButton theme={'secondary'} onClick={this.deleteEmail(address.email)}>{this.t('delete')}</IrmaButton>
+        }</td>
+      </tr>
+    );
+  }
 
-    render = () => {
-        return (<div><table>
-            {this.render_email_header()}
-            <tbody>
-                {this.props.emails.map(this.render_email_row)}
-            </tbody>
-        </table></div>);
-    }
+  render = () => {
+    return (
+      <div>
+        <table>
+          {this.renderEmailHeader()}
+          <tbody>
+          {this.props.emails.map(this.renderEmailRow)}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 Emails.propTypes = {
-    t: p.func.isRequired,
-    dispatch: p.func.isRequired,
-    emails: p.arrayOf(p.object.isRequired).isRequired,
+  t: p.func.isRequired,
+  dispatch: p.func.isRequired,
+  emails: p.arrayOf(p.object.isRequired).isRequired,
 };
 
 export default withTranslation(['emails', 'common'])(connect(mapStateToProps)(Emails));
