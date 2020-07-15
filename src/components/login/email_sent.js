79c5542bef663e5b549721d970f9120d39f7dbaf
Ivar Derksen <ivarderksen@hotmail.nl>
2020-07-15 16:31:42 +0200

    Move column to helper

--- src/components/login/email_sent.js
+++ src/components/login/email_sent.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import {withTranslation} from 'react-i18next';
-
-import '../../templates/column.scss';
 import IrmaAppBar from '../../helpers/irma_app_bar';
+import Column from '../../helpers/column';
 
 const EmailSent = (props) => {
   return (
     <>
       <IrmaAppBar title={props.t('title')}/>,
-      <div className={'column'}>
+      <Column>
         <p>
           {props.t('explanation')}
         </p>
-      </div>
+      </Column>
     </>
   );
 }
