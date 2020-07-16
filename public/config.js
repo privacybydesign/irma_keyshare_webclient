16c5a2409ba867202f1fe20389d5a30781738fa4
Ivar Derksen <ivarderksen@hotmail.nl>
2020-07-16 11:35:06 +0200

    Introduced explanation text email overview

--- public/config.js
+++ public/config.js
@@ -1,4 +1,8 @@
 window.config = {
   server: 'http://localhost:8080',
   lang: 'en',
+  emailIssuanceUrl: {
+    en: 'https://privacybydesign.foundation/issuance/email/',
+    nl: 'https://privacybydesign.foundation/uitgifte/email/',
+  },
 };
