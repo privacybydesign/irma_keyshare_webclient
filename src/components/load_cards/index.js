65e02a78453d11068d1393f1b36dc74b6dd4de20
Ivar Derksen <ivarderksen@hotmail.nl>
2020-07-17 11:31:12 +0200

    Introduced load cards component

---
+++ src/components/load_cards/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Trans, withTranslation} from 'react-i18next';
+
+const LoadCards = (props) => {
+  return (
+    <>
+      <h2>{props.t('header')}</h2>
+      <p>
+        <Trans
+          t={props.t}
+          i18nKey="explanation"
+          // eslint-disable-next-line
+          components={[ <a href={window.config.attributesOverviewUrl[props.i18n.language]} /> ]}
+        />
+      </p>
+    </>
+  );
+}
+
+export default withTranslation('load-cards')(LoadCards);
