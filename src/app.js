cab98ccbabc1e3ea0ea316f98018e074196827e1
Sietse Ringers <mail@sietseringers.net>
2021-10-20 14:13:49 +0200

    feat: translate <head> and html lang

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withTranslation } from 'react-i18next';
+
 import Login from './components/login/';
 import LoadingSpinner from './helpers/loading_spinner';
 import AccountOverview from './components/account_overview';
@@ -16,18 +18,25 @@ const mapStateToProps = state => {
 };
 
 // TODO: Maybe convert into router to improve URL structure.
-const App = (props) => {
-  if (props.loading) {
-    return <LoadingSpinner/>;
-  } else if (props.errorRaised) {
-    return <ErrorMessage/>;
-  } else if (props.loggedIn) {
-    return <AccountOverview/>;
-  } else if (props.registrationVerified) {
-    return <RegistrationVerified dispatch={props.dispatch}/>;
-  } else {
-    return <Login/>;
+class App extends React.Component {
+  componentDidMount() {
+    document.documentElement.setAttribute('lang', this.props.i18n.language);
+    document.title = this.props.t('title');
   }
-};
 
-export default connect(mapStateToProps)(App);
+  render() {
+    if (this.props.loading) {
+      return <LoadingSpinner/>;
+    } else if (this.props.errorRaised) {
+      return <ErrorMessage/>;
+    } else if (this.props.loggedIn) {
+      return <AccountOverview/>;
+    } else if (this.props.registrationVerified) {
+      return <RegistrationVerified dispatch={this.props.dispatch}/>;
+    } else {
+      return <Login/>;
+    }
+  }
+}
+
+export default connect(mapStateToProps)(withTranslation('app')(App));
