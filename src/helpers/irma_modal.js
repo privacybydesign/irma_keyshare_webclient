f90da7d47ff391bbe3f7c67a529d2a2256d205c2
Ivar Derksen <ivar.derksen@sidn.nl>
2021-11-12 14:51:52 +0100

    Feat: ask confirmation before an email address is deleted

--- src/helpers/irma_modal.js
+++ src/helpers/irma_modal.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import p from 'prop-types';
+import { withTranslation } from 'react-i18next';
 
 import './irma_modal.scss';
 import IrmaAppBar from './irma_app_bar';
 import Column from './column';
+import IrmaButton from "./irma_button";
 
 class IrmaModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.t = props.t;
+  }
+
   static propTypes = {
     theme: p.oneOf(['primary', 'secondary']).isRequired,
     title: p.string.isRequired,
+    action: p.string.isRequired,
+    onConfirm: p.func.isRequired,
+    onDismiss: p.func.isRequired,
   };
 
   render() {
@@ -20,6 +30,14 @@ class IrmaModal extends React.Component {
               <IrmaAppBar theme={this.props.theme} title={this.props.title}/>
               <Column>
                 {this.props.children}
+                <div className={'irma-modal-button-bar'}>
+                  <IrmaButton theme={'plain'} onClick={() => this.props.onDismiss()}>
+                    {this.t('cancel')}
+                  </IrmaButton>
+                  <IrmaButton theme={this.props.theme} onClick={() => this.props.onConfirm()}>
+                    {this.props.action}
+                  </IrmaButton>
+                </div>
               </Column>
             </div>
           </div>
@@ -29,4 +47,4 @@ class IrmaModal extends React.Component {
   }
 }
 
-export default IrmaModal;
+export default withTranslation('irma-modal')(IrmaModal);
